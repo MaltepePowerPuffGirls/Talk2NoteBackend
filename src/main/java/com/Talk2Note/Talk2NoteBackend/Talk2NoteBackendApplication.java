820b68e43374d0d2187910db0927b3aff75601a9
Orkun Kurul <orkunkurul@webinen.com>
2024-03-11 23:34:22 +0300

    project initialization

---
+++ src/main/java/com/Talk2Note/Talk2NoteBackend/Talk2NoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Talk2Note.Talk2NoteBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Talk2NoteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Talk2NoteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Talk2Note/Talk2NoteBackend/Talk2NoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Talk2Note.Talk2NoteBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Talk2NoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
