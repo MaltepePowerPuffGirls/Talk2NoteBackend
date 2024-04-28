8b4d8c928077f5438dc1243cf5b90f2e212dcfd7
furkaanpnr <furkanpnr220@gmail.com>
2024-04-28 14:48:20 +0300

    Stash applyed: Code recovered from an old branch named 'feature/note-crud-operations'

---
+++ src/main/java/com/Talk2Note/Talk2NoteBackend/api/dto/NoteStatusUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.Talk2Note.Talk2NoteBackend.api.dto;
+
+import com.Talk2Note.Talk2NoteBackend.core.enums.NoteStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoteStatusUpdateRequest {
+
+    @JsonProperty("note_status")
+    NoteStatus noteStatus;
+}
